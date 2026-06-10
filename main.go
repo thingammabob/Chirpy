df62a214ab2504d4994e2b77595c9585ecd0b012
thingammabob <86408122+thingammabob@users.noreply.github.com>
2026-06-10 23:34:51 +0530

    Add server health endpoint

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
+
+func healthyHandler(resWriter http.ResponseWriter, req *http.Request) {
+	req.Header.Set("Content-Type", "application/json")
+	resWriter.WriteHeader(200)
+	bodyBytes := []byte("OK")
+	resWriter.Write(bodyBytes)
+}
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -8,6 +17,7 @@ func main() {
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.HandleFunc("/healthz/", healthyHandler)
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	newServer.ListenAndServe()
 }
